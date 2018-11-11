a2bcf4b1b77ca38464f30ca5d9da616ce3050f1d
ainulya16 <ainulyaqinmuntako@gmail.com>
2018-11-12 06:22:34 +0700

    add status bar component

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import { Platform } from 'react-native'
+import { Platform, View } from 'react-native'
 import ScrollableTabView  from 'react-native-scrollable-tab-view'
 import TabBar from './components/tabBar'
 import Article from './components/article'
 import Book from './components/book'
+import styles from './styles';
+import { colors } from '../../styles';
+import { StatusBar } from '../../components'
 
 class Home extends Component{
     constructor(props){
@@ -13,14 +16,15 @@ class Home extends Component{
     render(){
         const paddingTop = Platform.select({ios:20,android:0})
         return (
-            
-            <ScrollableTabView 
-                style={{paddingTop}}
-                renderTabBar={()=><TabBar {...this.props}/>}
-            >
-              <Article tabLabel="Article" />
-              <Book tabLabel="Book" />
-            </ScrollableTabView>
+            <View style={[styles.container]}>
+                <StatusBar backgroundColor={colors.primary} barStyle='light-content'/>
+                <ScrollableTabView 
+                    renderTabBar={()=><TabBar {...this.props}/>}
+                >
+                    <Article tabLabel="Article" />
+                    <Book tabLabel="Book" />
+                </ScrollableTabView>
+            </View>
         );
     }
     
